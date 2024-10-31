d94eb1ab4138e045a2b6b45b1843b1a8bfb07241
jwzhang411898961 <jwzhang411898961>
2024-10-31 02:11:11 +0000

    put all test code in the adhoc_test folder

---
+++ ds-notebooks/adhoc_test/Rag.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %pip install lxml==4.9.3 transformers==4.30.2 langchain==0.0.344 databricks-vectorsearch==0.22
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
